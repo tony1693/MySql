43f7bf3fe78481167ca48f5aeda5b276eb57ae36
tony1693 <toak_16@hotmail.com>
2024-03-05 21:39:55 +0100

    Añadir Retos de Uniones-tablas  SQL

--- databaseConnection.js
+++ databaseConnection.js
@@ -41,3 +41,9 @@ const teachersSubject = 'SELECT subject_id, COUNT(*) AS num_teachers FROM teache
 const getNoteStudent = 'SELECT id, mark FROM marks WHERE (id BETWEEN 1 AND 20) OR (mark > 8 AND YEAR(date) = YEAR(CURDATE()) - 1)';
 const getAverageGrade = 'SELECT subject_id, AVG(mark) AS notes FROM marks WHERE YEAR(date) = YEAR(CURDATE()) - 1 GROUP BY subject_id';
 const arithmetic = 'SELECT student_id, AVG(dark) AS note FROM darks WHERE YEAR(date) = YEAR(CURDATE()) - 1 GROUP BY student_id';
+
+// ::RETOS DE UNIONES::
+
+const getInfo = 'SELECT students.first_name, students.last_name, subjects.title as subject FROM students JOIN subjects ON students.group_id = subjects.subject_id';
+const getNameSubject = 'SELECT teachers.firts_name, teachers.last_name, subjects.title as subject FROM teachers JOIN subject_teacher ON teachers.teacher_id = subject_teacher.teacher_id JOIN subjects ON subject_teacher.subject_id = subjects.subject_id';
+const selectInfo = 'SELECT COUNT(students.student_id) as total_students, subjects.title as subject, teachers.firts_name as teacher_first_name, teachers.last_name as teacher_last_name FROM students JOIN subjects ON students.group_id = subjects.subject_id JOIN subject_teacher ON subjects.subject_id = subject_teacher.subject_id JOIN teachers ON subject_teacher.teacher_id = teachers.teacher_id GROUP BY subjects.subject_id, teachers.teacher_id';
